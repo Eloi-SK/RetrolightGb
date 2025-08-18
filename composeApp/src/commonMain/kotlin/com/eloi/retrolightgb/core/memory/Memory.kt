e149cbf3c6c86ebe03a585f372fe8039854cfb1d
Eloi Matos <eloi_matos@hotmail.com>
2025-08-18 08:58:43 -0300

    chore: dump memory areas

--- composeApp/src/commonMain/kotlin/com/eloi/retrolightgb/core/memory/Memory.kt
+++ composeApp/src/commonMain/kotlin/com/eloi/retrolightgb/core/memory/Memory.kt
@@ -23,6 +23,41 @@ class Memory {
         ram[addr] = value
     }
 
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append("Memory Map:\n")
+        sb.append("  Boot ROM enabled: $isBootRomEnabled\n")
+        sb.append("  VRAM content:")
+        for (i in 0x8000 until 0xA000) {
+            if (i % 16 == 0) {
+                sb.append("\n    0x${i.toHexString()}: ")
+            }
+            sb.append("${ram[i].toInt()} ")
+        }
+        sb.append("\n  OAM content:")
+        for (i in 0xFE00 until 0xFEA0) {
+            if (i % 16 == 0) {
+                sb.append("\n    0x${i.toHexString()}: ")
+            }
+            sb.append("${ram[i].toInt()} ")
+        }
+        sb.append("\n  IO RAM content:")
+        for (i in 0xFF00 until 0xFF80) {
+            if (i % 16 == 0) {
+                sb.append("\n    0x${i.toHexString()}: ")
+            }
+            sb.append("${ram[i].toInt()} ")
+        }
+        sb.append("\n  H RAM content:")
+        for (i in 0xFF80 .. 0xFFFF) {
+            if (i % 16 == 0) {
+                sb.append("\n    0x${i.toHexString()}: ")
+            }
+            sb.append("${ram[i].toInt()} ")
+        }
+        return sb.toString()
+    }
+
     companion object {
         private const val RAM_SIZE = 0x10000 // 64KB
         private const val BOOT_DISABLED_ADDRESS = 0xFF50
