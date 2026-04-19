f11061b56e24a3326535303921ed3a996c7cad1d
Eloi Matos <eloi_matos@hotmail.com>
2026-04-19 15:18:47 -0300

    chore: resize window

--- composeApp/src/jvmMain/kotlin/com/eloi/retrolightgb/main.kt
+++ composeApp/src/jvmMain/kotlin/com/eloi/retrolightgb/main.kt
@@ -29,7 +29,7 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "RetrolightGb",
-        state = rememberWindowState(width = 800.dp, height = 600.dp)
+        state = rememberWindowState(width = 640.dp, height = 576.dp)
     ) {
         App()
     }
