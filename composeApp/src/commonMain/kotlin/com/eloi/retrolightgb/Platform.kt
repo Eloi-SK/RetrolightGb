5e24be59926cd154306423eb6028931218da3fc0
Eloi Matos <eloi_matos@hotmail.com>
2026-04-28 18:50:55 -0300

    chore: remove compose project example

--- composeApp/src/commonMain/kotlin/com/eloi/retrolightgb/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.eloi.retrolightgb
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/jvmMain/kotlin/com/eloi/retrolightgb/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package com.eloi.retrolightgb
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
