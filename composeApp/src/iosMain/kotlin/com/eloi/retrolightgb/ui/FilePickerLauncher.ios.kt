4ae01b0d93988081e2825c9b9a20327311aade06
Eloi Matos <eloi_matos@hotmail.com>
2026-04-29 12:24:09 -0300

    feat: ios file picker

--- composeApp/src/iosMain/kotlin/com/eloi/retrolightgb/ui/FilePickerLauncher.ios.kt
+++ composeApp/src/iosMain/kotlin/com/eloi/retrolightgb/ui/FilePickerLauncher.ios.kt
@@ -1,5 +1,47 @@
 package com.eloi.retrolightgb.ui
 
-actual class FilePickerLauncher(private val onResult: (ByteArray) -> Unit) {
-    actual fun launch() {}
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.readBytes
+import platform.Foundation.NSData
+import platform.Foundation.NSURL
+import platform.Foundation.dataWithContentsOfURL
+import platform.UIKit.UIDocumentPickerDelegateProtocol
+import platform.UIKit.UIDocumentPickerViewController
+import platform.UIKit.UIViewController
+import platform.UniformTypeIdentifiers.UTTypeItem
+import platform.darwin.NSObject
+
+actual class FilePickerLauncher(
+    private val viewController: UIViewController,
+    private val onResult: (ByteArray) -> Unit
+) {
+    private var delegate: DocumentPickerDelegate? = null
+
+    actual fun launch() {
+        val picker = UIDocumentPickerViewController(forOpeningContentTypes = listOf(UTTypeItem))
+        picker.allowsMultipleSelection = false
+        delegate = DocumentPickerDelegate(onResult)
+        picker.delegate = delegate
+        viewController.presentViewController(picker, animated = true, completion = null)
+    }
+}
+
+private class DocumentPickerDelegate(
+    private val onResult: (ByteArray) -> Unit
+) : NSObject(), UIDocumentPickerDelegateProtocol {
+
+    @OptIn(ExperimentalForeignApi::class)
+    override fun documentPicker(
+        controller: UIDocumentPickerViewController,
+        didPickDocumentsAtURLs: List<*>
+    ) {
+        val url = didPickDocumentsAtURLs.firstOrNull() as? NSURL ?: return
+        url.startAccessingSecurityScopedResource()
+        try {
+            val data = NSData.dataWithContentsOfURL(url) ?: return
+            onResult(data.bytes!!.readBytes(data.length.toInt()))
+        } finally {
+            url.stopAccessingSecurityScopedResource()
+        }
+    }
 }
--- composeApp/src/iosMain/kotlin/com/eloi/retrolightgb/ui/compose/FilePickerLauncher.ios.kt
+++ composeApp/src/iosMain/kotlin/com/eloi/retrolightgb/ui/compose/FilePickerLauncher.ios.kt
@@ -3,7 +3,21 @@ package com.eloi.retrolightgb.ui.compose
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import com.eloi.retrolightgb.ui.FilePickerLauncher
+import platform.UIKit.UIApplication
+import platform.UIKit.UIViewController
 
 @Composable
-actual fun rememberFilePickerLauncher(onResult: (ByteArray) -> Unit): FilePickerLauncher =
-    remember { FilePickerLauncher(onResult) }
+actual fun rememberFilePickerLauncher(onResult: (ByteArray) -> Unit): FilePickerLauncher {
+    return remember {
+        FilePickerLauncher(viewController = topViewController(), onResult = onResult)
+    }
+}
+
+private fun topViewController(): UIViewController {
+    var vc = UIApplication.sharedApplication.keyWindow?.rootViewController
+        ?: error("No root view controller found")
+    while (vc.presentedViewController != null) {
+        vc = vc.presentedViewController!!
+    }
+    return vc
+}
